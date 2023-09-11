5a503640367bf8f608205404cf3d3760c379dc5d
lycia <lycianex27@gmail.com>
2023-09-11 10:17:07 +0200

    icons
    linked to google icon

--- src/app.js
+++ src/app.js
@@ -10,11 +10,15 @@ h3.innerHTML = `${day},${hours}:${minutes}`;
 function displayWeatherCondition(response) {
   document.querySelector("h1").innerHTML = response.data.name;
   document.querySelector("h4").innerHTML = Math.round(response.data.main.temp);
-
+  icon.setAttribute(
+    "src",
+    ` http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
   document.querySelector("section").innerHTML = response.data.weather[0].main;
 }
 function searchCity(city) {
   let apiKey = "ebce50d94ede40a4e61876fb290cd8e4";
+
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
@@ -33,7 +37,7 @@ function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
-
+let iconElement = document.querySelector("icon");
 let h5 = document.querySelector("h5");
 h5.addEventListener("submit", handleSubmit);
 
